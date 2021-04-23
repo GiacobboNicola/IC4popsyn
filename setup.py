13aadf5e28a9e4d3149165529e6da23f771f8985
GNicola <giacobbo.nicola@gmail.com>
2021-04-23 23:16:38 +0200

    preparing a setup.py to build the package

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
-from setuptools import setup
+import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-setup(
-    name='ic4popsyn',
-    version='1.0.0',
-    author='Nicola Giacobbo',
-    author_email='giuliano.iorio@unibo.it',
-    url='https://github.com/GiacobboNicola/IC4popsyn',
-    packages=['ic4popsyn',],
-    install_requires=['numpy','pandas','progressbar'],
+setuptools.setup(
+    name="ic4popsyn-pkg-GNicola",
+    version="0.1",
+    author="Nicola Giacobbo",
+    author_email="giacobbo.nicola@gmail.com",
+    description="A package to help build initial conditions for population-synthesis codes.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GiacobboNicola/IC4popsyn",
+    project_urls={
+        "Bug Tracker": "https://github.com/GiacobboNicola/IC4popsyn/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
 )
-
-
