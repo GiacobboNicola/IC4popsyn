3ea5bc03b89c5f1ea23ba6f091fa651f891c0b99
giulianoiorio <giuliano.iorio89@gmail.com>
2021-09-28 14:15:08 +0200

    fixed a bug in setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-import setuptools
+from setuptools import setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="ic4popsyn-pkg-GNicola",
-    version="0.1",
-    author="Nicola Giacobbo",
+setup(
+    name='ic4popsyn',
+    version='0.1',
+    author='Nicola Giacobbo',
     author_email="giacobbo.nicola@gmail.com",
     description="A package to help build initial conditions for population-synthesis codes.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/GiacobboNicola/IC4popsyn",
+    url='https://github.com/GiacobboNicola/IC4popsyn',
     project_urls={
         "Bug Tracker": "https://github.com/GiacobboNicola/IC4popsyn/issues",
     },
@@ -19,7 +19,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: MIT License",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.7",
+    packages=['ic4popsyn',],
+    install_requires=['numpy','pandas','progressbar'],
+    python_requires=">=3.7"
 )
